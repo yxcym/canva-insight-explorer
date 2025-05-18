a949831baa24915a7fc7fedc1b6265ab2c4bb3af
杨泽瑞 <yangzerui2021@163.com>
2025-05-19 01:51:56 +0800

    完成测试页面初始 HTML 结构搭建并实现翻转盒交互功能

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,36 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+ <div class="blind-box" onclick="this.classList.toggle('flipped')">
+  <div class="front">
+    <img src="https://gitee.com/yxcym/images/raw/master/img/%E3%83%95%E3%83%AA%E3%83%BC%E3%83%95%E3%82%A9%E3%83%BC%E3%83%AB%20.jpg" alt="盲盒封面">
+  </div>
+  <div class="back">
+    <button>开始测试</button>
+  </div>
+</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+.blind-box {
+  width: 300px; height: 400px;
+  perspective: 1000px;
+  cursor: pointer;
 }
+.blind-box > div {
+  width: 100%; height: 100%;
+  position: absolute;
+  backface-visibility: hidden;
+  transition: transform 0.8s cubic-bezier(0.175, 0.885, 0.32, 1.275);
+}
+.front { background: #165DFF; } /* Canva蓝色 */
+.back { 
+  background: #00B42A; /* Canva绿色 */
+  transform: rotateY(180deg);
+  display: flex; justify-content: center; align-items: center;
+}
+.flipped .front { transform: rotateY(180deg); }
+.flipped .back { transform: rotateY(0); }
 </style>
